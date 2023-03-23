9ec75aae89b8decf385a261d39ed51331ddad7ba
Nawrasse Dahman <nawrassedhm@techie.com>
2023-03-23 14:19:47 +0000

    Update index.tsx

--- src/layout/Home/index.tsx
+++ src/layout/Home/index.tsx
@@ -5,27 +5,24 @@ import { Favorites } from './favorites/FavoritesSection'
 import { History } from './History'
 import { LinkTooltip } from './LinkTooltip'
 import { Projects } from './Projects'
-import { Writing } from './Writing'
+
 
 export const HomeLayout = () => (
   <Layout>
     <SlideFade in={true} offsetY="20px">
       <Heading
-        as="h1"
+        as="h3"
         pt="8"
         fontSize={['32px', '48px', '64px']}
         lineHeight={1.2}
       >
-        <LinkTooltip name="intro">Hi, I&apos;m Ross.</LinkTooltip> A product
-        designer, full-stack engineer, and systems thinker that loves making
-        things for the web.
+        <LinkTooltip name="intro">Hi, I&apos;m Nawrasse.</LinkTooltip> A Full-Stack Developer And An Author.
       </Heading>
       <DashDivider my="60px" />
       <History />
       <DashDivider my="60px" />
       <Projects />
-      <DashDivider my="60px" />
-      <Writing />
+
       <DashDivider my="60px" />
       <Favorites />
     </SlideFade>
