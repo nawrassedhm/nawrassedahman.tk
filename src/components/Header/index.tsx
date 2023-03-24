8d298e1afb0a230e4b3c47a2bae934759671cf71
Nawrasse Dahman <nawrassedhm@techie.com>
2023-03-24 14:12:57 +0000

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -23,7 +23,7 @@ export const Header = () => {
     >
       <Logo />
       <Flex gap="8" align="center">
-        <Link href='https://chakra-ui.com' isExternal>
+        <Link href='#' isExternal>
           Writing
         </Link>
         <Divider orientation="vertical" h="5" borderColor="divider" />
