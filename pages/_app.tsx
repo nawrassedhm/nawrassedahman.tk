c0a57044f06c8af0d2b6f39dc8d79ef8fdcb8230
Nawrasse Dahman <nawrassedhm@techie.com>
2023-03-24 11:52:06 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,46 @@
-import { DefaultSeo, ThemeProvider } from 'components'
-import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import React, { useEffect } from 'react'
-import * as gtag from 'utils/analytics'
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { NextSeo } from "next-seo";
 import '../public/fonts.css'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
+import { ThemeProvider } from 'components'
 
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
+const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <React.Fragment>
-      <DefaultSeo />
-      <ThemeProvider>
-       <Component/>
-      </ThemeProvider>
-    </React.Fragment>
-  )
-}
+
+      <NextSeo
+        title="Lazar Nikolov - Developer, designer, course creator."
+        description="Full-stack Engineer, UI Designer, and Open Source Advocate."
+        twitter={{
+          cardType: "summary_large_image",
+          handle: "@NikolovLazar",
+        }}
+        openGraph={{
+          url: "https://nikolovlazar.com",
+          title: "Lazar Nikolov - Developer, designer, course creator.",
+          description:
+            "Full-stack Engineer, UI Designer, and Open Source Advocate.",
+          locale: "en_US",
+          images: [
+            {
+              url: "https://nikolovlazar.com/assets/images/social.png",
+              width: 1200,
+              height: 630,
+              alt: "Lazar Nikolov",
+              type: "image/png",
+            },
+          ],
+        }}
+      />
+       <ThemeProvider>
+        <Layout>
+
+          <Component {...pageProps} />
+        </Layout>
+       </ThemeProvider>
+
+
+  );
+};
+
+export default App;
