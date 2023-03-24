ad53f27b57db0b5efd4c100c546f1e058a25764b
Nawrasse Dahman <nawrassedhm@techie.com>
2023-03-24 12:28:44 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { NextSeo } from "next-seo";
 import '../public/fonts.css';
 import { ThemeProvider } from 'components'
-import { DefaultSeo, ThemeProvider } from 'components'
 import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import React, { useEffect } from 'react'
 import * as gtag from 'utils/analytics'
 import '../public/fonts.css'
 
