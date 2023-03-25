0d04852bbf666eb26e0e0107ea617cfd7c2c55db
Nawrasse Dahman <nawrassedhm@techie.com>
2023-03-25 01:06:37 +0000

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-import { GA_TRACKING_ID } from 'utils/analytics'
 
 export default class MyDocument extends Document {
   render() {
@@ -8,21 +7,6 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', '${GA_TRACKING_ID}', {
-                        page: window.location.pathname
-                    });`,
-            }}
-          />
         </Head>
         <body>
           <Main />
